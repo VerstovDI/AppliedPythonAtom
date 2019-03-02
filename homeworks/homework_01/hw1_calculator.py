3ccde1b01b6f9d048e637b9e722b027bdc9f1411
Danila <VerstovDI@yandex.ru>
2019-03-02 20:55:30 +0300

    First calculator

--- homeworks/homework_01/hw1_calculator.py
+++ homeworks/homework_01/hw1_calculator.py
@@ -11,3 +11,12 @@ def calculator(x, y, operator):
     :return: результат операции или None, если операция не выполнима
     '''
     raise NotImplementedError
+    if operator == "plus":
+        return obj_1+obj_2
+    if operator == "minus":
+        return obj_1-obj_2
+    if operator == "mult":
+        return obj_1*obj_2
+    if operator == "divide":
+        return obj_1/obj_2 if obj_2 != 0 else None
+    return None
