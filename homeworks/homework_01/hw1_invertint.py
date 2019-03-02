5b82901683e8928bad7b72dcf6201508c04b1d88
Danila <VerstovDI@yandex.ru>
2019-03-02 21:32:29 +0300

    First_invertint

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -10,3 +10,20 @@ def reverse(number):
     :return: инвертированное число
     '''
     raise NotImplementedError
+    list = []
+    if num < 0:
+        fl = True
+    else:
+        fl = False
+    num = abs(num)
+    if num == 0:
+        return 0
+    while num >= 1:
+        tmp = num%10
+        list.append((tmp))
+        num = num//10
+    list = [str(i) for i in list]
+    res = "".join(list)
+    if fl:
+        return -1*int(res)
+    return int(res)
