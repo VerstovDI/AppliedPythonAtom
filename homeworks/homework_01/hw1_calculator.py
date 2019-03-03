2b4a787ae0d9cd04d5013aa0cc0373a7195841de
Danila <VerstovDI@yandex.ru>
2019-03-03 10:41:04 +0300

    Not implented - int the end. Use x,y instead of my obj_1, obj_2; include isinsistance() - type check

--- homeworks/homework_01/hw1_calculator.py
+++ homeworks/homework_01/hw1_calculator.py
@@ -10,13 +10,16 @@ def calculator(x, y, operator):
     :param operator: 4 оператора: plus, minus, mult, divide
     :return: результат операции или None, если операция не выполнима
     '''
-    if operator == "plus":
-        return x + y
-    if operator == "minus":
-        return x - y
-    if operator == "mult":
-        return x * y
-    if operator == "divide":
-        return x / y if y != 0 else None
+    if isinstance(x, (int, float)) and isinstance(y, (int, float)) and isinstance(operator, str):
+        if operator == "plus":
+            return x + y
+        if operator == "minus":
+            return x - y
+        if operator == "mult":
+            return x * y
+        if operator == "divide":
+            return x / y if y != 0 else None
+    else:
+        return None
     return None
     raise NotImplementedError
