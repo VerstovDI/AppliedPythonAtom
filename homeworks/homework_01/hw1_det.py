e62ac2f20c914b41e6d178fd412bc9d163fe5e68
Danila <VerstovDI@yandex.ru>
2019-03-03 23:11:22 +0300

    Det. Change return

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -12,11 +12,14 @@ def calculate_determinant(list_of_lists):
     :return: значение определителя или None
     '''
     size = len(list_of_lists)
+
     if len(list_of_lists) <= 1:
         return None
+
     global i
     global t
     t = 0
+
     if not isinstance(list_of_lists[t], list):
         return None
 
@@ -25,17 +28,21 @@ def calculate_determinant(list_of_lists):
             return None
 
     swap_cnt = 1
+
     for i in range(0, size):
         global j
         global k
         global supp_ind
         supp_ind = i
         for j in range(i + 1, size):
+
             if abs(list_of_lists[j][i]) > abs(list_of_lists[supp_ind][i]):
                 supp_ind = j
                 fl = True
+
             else:
                 fl = False
+
             if fl:
                 for k in range(0, size):
                     buf = list_of_lists[i][k]
@@ -43,6 +50,7 @@ def calculate_determinant(list_of_lists):
                     list_of_lists[supp_ind][k] = buf
                 if i != supp_ind:
                     swap_cnt = - swap_cnt * 1
+
                 else:
                     swap_cnt = - swap_cnt * -1
 
@@ -51,10 +59,13 @@ def calculate_determinant(list_of_lists):
                     coeff = -list_of_lists[j][i] / list_of_lists[i][i]
                     for k in range(size - 1, i - 1, -1):
                         list_of_lists[j][k] += list_of_lists[i][k] * coeff
-    global res
+
     res = 1
     for i in range(0, size):
         res *= list_of_lists[i][i]
 
-    return res * swap_cnt
+    if isinstance(res * swap_cnt, (float, int)):
+        return res * swap_cnt
+    else:
+        return None
     raise NotImplementedError
