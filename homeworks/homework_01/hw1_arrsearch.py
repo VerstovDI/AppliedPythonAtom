e6e7aa889ca729319f059df9213747f9d08fb909
Danila <VerstovDI@yandex.ru>
2019-03-04 22:09:10 +0300

    First arrsearch

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -13,4 +13,23 @@ def find_indices(input_list, n):
     :param n: целевая сумма
     :return: tuple из двух индексов или None
     '''
+    """
+    Поскольку необходимо время O(len(input_list)),
+    логично воспользоваться хэш-таблицей (время доступа!) -> словарь
+    Данный алгоритм выполняетcя за требуемое время,
+    однако вместе с тем необходимо памяти также O(len(input_list))
+       1) Скопируем массив в словарь
+       2) Будем проходить массив и оценивать n - input_list[i]
+    """
+    buf_d = {}  # Создаём пустой словарь
+    for cnt in range(0, len(input_list)):  # Перегоняем массив в словарь поиндексно
+        buf_d[input_list[cnt]] = cnt  # Ключ - число, значение - индекс
+
+    t = 0
+    for item in input_list:
+        temp = buf_d.get(n - item)
+        if temp and temp and n - item != item:
+            return tuple([t, temp])
+        t += 1
+    return None
     raise NotImplementedError
