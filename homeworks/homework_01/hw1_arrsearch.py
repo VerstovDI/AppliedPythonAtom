26a295ec8e214b72376b4599f04460c512d6711c
Danila <VerstovDI@yandex.ru>
2019-03-05 20:48:49 +0300

    First Arrsearch

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -3,9 +3,9 @@
 
 
 def find_indices(input_list, n):
-    buf_d = {}  # Создаём пустой словарь
-    for cnt in range(0, len(input_list)):  # Перегоняем в словарь поиндексно
-        buf_d[input_list[cnt]] = cnt  # Ключ - число, значение - индекс
+    buf_d = {}
+    for cnt in range(0, len(input_list)):
+        buf_d[input_list[cnt]] = cnt
     t = 0
     for item in input_list:
         temp = buf_d.get(n - item)
@@ -13,4 +13,3 @@ def find_indices(input_list, n):
             return tuple([t, temp])
         t += 1
     return None
-    raise NotImplementedError
