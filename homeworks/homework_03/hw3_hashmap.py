fa361c382cf35c779e46dbfc5b2d0562b40b92db
Danila <VerstovDI@yandex.ru>
2019-03-17 16:42:32 +0300

    HashMap

--- homeworks/homework_03/hw3_hashmap.py
+++ homeworks/homework_03/hw3_hashmap.py
@@ -3,80 +3,104 @@
 
 
 class HashMap:
-    '''
-    Давайте сделаем все объектненько,
-     поэтому внутри хешмапы у нас будет Entry
-    '''
-    class Entry:
+    # Информацию необходимо скрывать и защищать. Поэтому __ для атрибутов
+    __CONST_THRESHOLD = 0.75  # Константа - предел заполненности HashMap
+
+    class Entry:  # Данные необходимо скрывать и защищать. Поэтому __
         def __init__(self, key, value):
-            '''
-            Сущность, которая хранит пары ключ-значение
-            :param key: ключ
-            :param value: значение
-            '''
+            self.__hkey = key  # Ключ Entry
+            self.__hval = value  # Значение Entry
 
-        def get_key(self):
-            # TODO возвращаем ключ
-            raise NotImplementedError
+        def get_key(self):  # Геттер ключа
+            return self.__hkey
 
-        def get_value(self):
-            # TODO возвращаем значение
-            raise NotImplementedError
+        def get_value(self):  # Геттер значения
+            return self.__hval
 
-        def __eq__(self, other):
-            # TODO реализовать функцию сравнения
-            raise NotImplementedError
+        def set_value(self, value):  # Cеттер значения
+            self.__hval = value
+
+        def __eq__(self, other):  # Cравнение по ключу
+            return self.get_key() == other.get_key()
 
     def __init__(self, bucket_num=64):
-        '''
-        Реализуем метод цепочек
-        :param bucket_num: число бакетов при инициализации
-        '''
-        raise NotImplementedError
+        self.__cur_sz = bucket_num  # Текущий размер HashMap
+        # Структура: cписок списков Entry
+        self.__htable = [[] for i in range(self.__cur_sz)]
+        self.__buckets = 0  # Количество непустых ячеек (bucket's)
+        # Процент заполненности таблицы.
+        # Если (self.__load_factor >= __CONST_THRESHOLD) -> resize
+        self.__load_factor = self.__buckets / self.__cur_sz
 
-    def get(self, key, default_value=None):
-        # TODO метод get, возвращающий значение,
-        #  если оно присутствует, иначе default_value
-        raise NotImplementedError
+    def get(self, key, default_value=None):  # Возвращает значение по ключу
+        ind = self._get_index(self._get_hash(key))
+        if ind is None:
+            return default_value
+        else:
+            for item in self.__htable[ind]:
+                if item.get_key() == key:
+                    return item.get_value()
+        return default_value
 
-    def put(self, key, value):
-        # TODO метод put, кладет значение по ключу,
-        #  в случае, если ключ уже присутствует он его заменяет
-        raise NotImplementedError
+    def put(self, key, value):  # Вставка в HashMap
+        ind = self._get_index(self._get_hash(key))
+        if ind is not None:
+            fl = False
+            for item in self.__htable[ind]:
+                if item.get_key() == key:
+                    item.set_value(value)
+                    fl = True
+                    break
+            if fl is False:
+                self.__htable[ind].append(self.Entry(key, value))
+                self.__buckets += 1
+        else:
+            print("Invalid key")
+        if self.__load_factor >= self.__CONST_THRESHOLD:
+            self._resize()
 
-    def __len__(self):
-        # TODO Возвращает количество Entry в массиве
-        raise NotImplementedError
+    def __len__(self):  # Возвращает длину списка списков (Hash table)
+        return self.__buckets
 
-    def _get_hash(self, key):
-        # TODO Вернуть хеш от ключа,
-        #  по которому он кладется в бакет
-        raise NotImplementedError
+    def _get_hash(self, key):  # Возвращает хэш (встроенная хэш-ф-я)->int
+        return hash(key)
 
-    def _get_index(self, hash_value):
-        # TODO По значению хеша вернуть индекс элемента в массиве
-        raise NotImplementedError
+    def _get_index(self, hash_value):  # Возвращает индекс по хэшу
+        return hash_value % self.__cur_sz
 
-    def values(self):
-        # TODO Должен возвращать итератор значений
-        raise NotImplementedError
+    def values(self):  # Итератор значений
+        __values = list()
+        for bucket in self.__htable:
+            for it in bucket:
+                __values.append(it.get_value())
+        return __values
 
-    def keys(self):
-        # TODO Должен возвращать итератор ключей
-        raise NotImplementedError
+    def keys(self):  # Итератор ключей
+        __keys = list()
+        for bucket in self.__htable:
+            for it in bucket:
+                __keys.append(it.get_key())
+        return __keys
 
-    def items(self):
-        # TODO Должен возвращать итератор пар ключ и значение (tuples)
-        raise NotImplementedError
+    def items(self):  # Итератор пар ключ-значение
+        __it, __bucket_list = list(), list()
+        for bucket in self.__htable:
+            __it += [(it.get_key(), it.get_value()) for it in bucket]
+        return __it
 
-    def _resize(self):
-        # TODO Время от времени нужно ресайзить нашу хешмапу
-        raise NotImplementedError
+    def _resize(self):  # resize по capacity
+        self.__cur_sz *= 2
+        old_htable = self.items()
+        self.__htable = [[] for i in range(self.__cur_sz)]
+        for item in old_htable:
+            self.put(item[0], item[1])
 
-    def __str__(self):
-        # TODO Метод выводит "buckets: {}, items: {}"
-        raise NotImplementedError
+    def __str__(self):  # Вывод
+        print("buckets: {}, items: {}".format(self.__buckets, self.items()))
 
-    def __contains__(self, item):
-        # TODO Метод проверяющий есть ли объект (через in)
-        raise NotImplementedError
+    def __contains__(self, item):  # check: in ?
+        for i in self.__htable:
+            for j in i:
+                if j.get_key() == item:
+                    return True
+        return False
