063b4df92cddc461ef86d51f9b591bd73e4296fd
Danila <VerstovDI@yandex.ru>
2019-04-29 20:10:18 +0300

    knns

--- homeworks/homework_08_ml/ml_classes/knns.py
+++ homeworks/homework_08_ml/ml_classes/knns.py
@@ -2,6 +2,10 @@
 # coding: utf-8
 
 
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+
 class KNNRegressor:
     """
     Построим регрессию с помощью KNN. Классификацию писали на паре
@@ -12,34 +16,39 @@ class KNNRegressor:
         Конструктор
         :param n: число ближайших соседей, которые используются
         '''
-        raise NotImplementedError
+        self._n = n
 
     def fit(self, X, y):
         '''
-        :param X: обучающая выборка, матрица размерности (num_obj, num_features)
-        :param y: целевая переменная, матрица размерности (num_obj, 1)
+        :param X: обучающая выборка,
+        матрица размерности (num_obj, num_features)
+        :param y: целевая переменная,
+        матрица размерности (num_obj, 1)
         :return: None
         '''
-        raise NotImplementedError
+        self._X = X
+        self._y = y
 
     def predict(self, X):
         '''
-        :param X: выборка, на которой хотим строить предсказания (num_test_obj, num_features)
+        :param X: выборка, на которой
+        хотим строить предсказания (num_test_obj, num_features)
         :return: вектор предсказаний, матрица размерности (num_test_obj, 1)
         '''
-
-        raise NotImplementedError
+        std_scaler = StandardScaler()
+        X_norm = std_scaler.fit_transform(X)
 
         y = []
-        assert len(X.shape) == 2
-        for t in X:
+        assert len(X_norm.shape) == 2
+        for curr in X_norm:
             # Посчитаем расстояние от всех элементов в тренировочной выборке
             # до текущего примера -> результат - вектор размерности трейна
-            # TODO d =
+            dist = np.sum(np.sqrt((self._X - curr)**2, axis=1))
             # Возьмем индексы n элементов, расстояние до которых минимально
             # результат -> вектор из n элементов
-            # TODO idx =
-            # TODO
-            prediction = None
+            idx = np.argsort(dist)[:self._n]
+            q = 1/dist[idx]  # Взвешенный способ
+            prediction = np.average(self._y[idx], weights=q)
             y.append(prediction)
+        self.is_fitted = True
         return y
